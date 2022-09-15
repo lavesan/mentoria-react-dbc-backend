1028910910a4752c2346c2d3e9f4fc8cb2c9a1b8
Valdery Alves <valdery.jur@gmail.com>
2022-09-15 20:10:25 -0300

    feat: removes unused entities
    Signed-off-by: Valdery Alves <valdery.jur@gmail.com>

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,14 +1,12 @@
 import { DataSource } from "typeorm";
 import { User } from "./src/modules/accounts/entities/User";
-import { Specification } from "./src/modules/cars/entities/Specification";
-import { Category } from "./src/modules/cars/entities/Category";
 
 const AppDataSource = new DataSource({
   type: "sqlite",
   database: "./src/database/database.sqlite",
   subscribers: [],
   migrations: ["./src/database/migration/*.ts"],
-  entities: [User, Specification, Category],
+  entities: [User],
 });
 
 export function createConnection(): Promise<DataSource> {
--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,21 +1,6 @@
 import { container } from "tsyringe";
 import { UserRepository } from "../../modules/accounts/repositories/implementations/UserRepository";
 import { IUserRepository } from "../../modules/accounts/repositories/IUserRepository";
-import { ICategoryRepository } from "../../modules/cars/repositories/ICategoryRepository";
-import { CategoryRepository } from "../../modules/cars/repositories/implementations/CategoriesRepository";
-import { SpecificationRepository } from "../../modules/cars/repositories/implementations/SpecificationRepository";
-import { ISpecificationRepository } from "../../modules/cars/repositories/ISpecificationRepository";
 
-// ICategoryRepository
-container.registerSingleton<ICategoryRepository>(
-  "CategoryRepository",
-  CategoryRepository
-);
-
-// ISpecificationRepository
-container.registerSingleton<ISpecificationRepository>(
-  "SpecificationRepository",
-  SpecificationRepository
-);
-
+// IUserRepository
 container.registerSingleton<IUserRepository>("UserRepository", UserRepository);
